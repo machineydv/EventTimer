5b1a07140c76cc8a584580a3b2eea69c1f48c115
machinexa <machinexa@gmail.com>
2020-09-15 15:22:36 +0545

    03:22:36 PM 09/15/2020

--- EventTimer.py
+++ EventTimer.py
@@ -10,7 +10,13 @@ from lib.Globals import *
 current_time = seconds_from_time(datetime.now().strftime("%H:%M:%S"))
 current_date = seconds_from_date(date.today().isoformat()) 
 current_seconds = current_time + current_date
-event_seconds = int(seconds_from_date(event_date)) + int(seconds_from_time(event_time)) + int(gmt_to_ktm)
+if event_timeformat == "GMT" or event_timeformat == "UTC": #UTC Time
+    event_seconds = int(seconds_from_date(event_date)) + int(seconds_from_time(event_time)) + int(gmt_to_ktm)
+elif event_timeformat == "PCT": #Pacific Time
+    event_seconds = int(seconds_from_date(event_date)) + int(seconds_from_time(event_time)) + int(pacific_to_ktm)
+elif event_timeformat == "EST": #Eastern Time
+    event_seconds = int(seconds_from_date(event_date)) + int(seconds_from_time(event_time)) + int(est_to_ktm)
+
 
 date, seconds = date_from_seconds(event_seconds-current_seconds)
 counter_seconds = seconds_from_date(date) + int(seconds)
--- lib/Globals.py
+++ lib/Globals.py
@@ -1,11 +1,14 @@
 from lib.ColoredObject import Color
 from lib.TimeFunctions import seconds_from_time
 
-event_name = 'HTB Compromised Box Release'  #Event Name
-event_date = '2020-09-12'   #Event Date
+event_name = 'BsidesBOS CTF'  #Event Name
+event_date = '2020-09-26'   #Event Date
 event_time = '19:00:00' #Event Time
+event_timeformat = "GMT" #Event Timeformat
 
 pacific_to_ktm = seconds_from_time('0:45:00') #For Nepal
-gmt_to_ktm = seconds_from_time('5:45:00')   #For Nepal
+gmt_to_ktm = seconds_from_time('5:45:00') #For Nepal
+est_to_ktm = seconds_from_time('04:00:00')
+est_to_ktm += seconds_from_time('5:45:00') #For Nepal
 
 ColorObj = Color()
