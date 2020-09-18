270feda5e39e89a77aa5b566f4009a4ec4b00158
machinexa <machinexa@gmail.com>
2020-09-18 09:51:05 +0545

    09:51:05 AM 09/18/2020

--- lib/PrintFunctions.py
+++ lib/PrintFunctions.py
@@ -5,13 +5,14 @@ from termcolor import colored
 from lib.Globals import *
 from lib.TimeFunctions import *
 
-def print_from_seconds(second):
+def print_from_seconds(second, edict):
     system('clear')
     while second > 2:
         nepal = colored(time_from_seconds(second), color='cyan')
         india = colored(time_from_seconds(second-seconds_from_time('00:15:00')), color='yellow')
         universal = colored(time_from_seconds(second-seconds_from_time('05:45:00')), color='red')
         print("\x1b[%d;%dH" % (1, 1), end="")
+        print(f"{ColorObj.information} Event: {edict['event_name']}")
         print(f"{ColorObj.good} Nepali Time: {nepal}, Indian Time: {india}, Universal Time: {universal}")
         sleep(1)
         second -= 1
