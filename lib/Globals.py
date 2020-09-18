206346e6a5431f95ea113721e639cc288da8324f
machinexa <machinexa@gmail.com>
2020-09-18 09:42:26 +0545

    09:42:26 AM 09/18/2020

--- lib/Globals.py
+++ lib/Globals.py
@@ -7,6 +7,12 @@ event_1 = {
     "event_time":"19:00:00",            #Event Time
     "event_timeformat":"EST"            #Event Timeformat
 }
+event_2 = {
+    "event_name":"BsidesBOS CTF",       #Event Name
+    "event_date":"2020-09-26",          #Event Date
+    "event_time":"19:00:00",            #Event Time
+    "event_timeformat":"EST"            #Event Timeformat
+}
 
 pacific_to_ktm = seconds_from_time('0:45:00') #For Nepal
 gmt_to_ktm = seconds_from_time('5:45:00') #For Nepal
--- lib/PrintFunctions.py
+++ lib/PrintFunctions.py
@@ -7,15 +7,12 @@ from lib.TimeFunctions import *
 
 def print_from_seconds(second):
     system('clear')
-    try:
-        while second > 2:
-            nepal = colored(time_from_seconds(second), color='cyan')
-            india = colored(time_from_seconds(second-seconds_from_time('00:15:00')), color='yellow')
-            universal = colored(time_from_seconds(second-seconds_from_time('05:45:00')), color='red')
-            print("\x1b[%d;%dH" % (1, 1), end="")
-            print(f"{ColorObj.good} Nepali Time: {nepal}, Indian Time: {india}, Universal Time: {universal}")
-            sleep(1)
-            second -= 1
-    except Exception as E:
-        pass
-    return "{} {}".format(ColorObj.good,time_from_seconds(second))
+    while second > 2:
+        nepal = colored(time_from_seconds(second), color='cyan')
+        india = colored(time_from_seconds(second-seconds_from_time('00:15:00')), color='yellow')
+        universal = colored(time_from_seconds(second-seconds_from_time('05:45:00')), color='red')
+        print("\x1b[%d;%dH" % (1, 1), end="")
+        print(f"{ColorObj.good} Nepali Time: {nepal}, Indian Time: {india}, Universal Time: {universal}")
+        sleep(1)
+        second -= 1
+    return True
