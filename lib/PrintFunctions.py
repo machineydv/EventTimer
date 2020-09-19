252031ef41437d20f1582be8fe1420df76003f39
machinexa <machinexa@gmail.com>
2020-09-19 19:50:39 +0545

    07:50:39 PM 09/19/2020

--- lib/PrintFunctions.py
+++ lib/PrintFunctions.py
@@ -5,13 +5,15 @@ from lib.Globals import *
 from lib.TimeFunctions import *
 
 def print_from_seconds(second, edict):
-    Nepal = colored(time_from_seconds(second), color='cyan')
-    India = colored(time_from_seconds(second-seconds_from_time('00:15:00')), color='yellow')
-    Universal = colored(time_from_seconds(second-seconds_from_time('05:45:00')), color='red')
+    india_diff = seconds_from_time('00:15:00')
+    gmt_diff = seconds_from_time('05:45:00')
+    nepal = colored(time_from_seconds(second), color='cyan')
+    india = colored(time_from_seconds(second-india_diff), color='yellow')
+    universal = colored(time_from_seconds(second-gmt_diff), color='red')
     if 'event_description' in edict.keys():
         print(f"{ColorObj.information} Event {colored(edict['event_name'], color='green', attrs=['bold'])}: {edict['event_description']}")
     else:
         print(f"{ColorObj.information} Event: {colored(edict['event_name'], color='green', attrs=['bold'])}")
-    print(f"{ColorObj.good} Universal Time: {Universal}, Nepali Time: {Nepal}, Indian Time: {India} ")
+    print(f"{ColorObj.good} Universal/UTC Time: {universal}, Nepali Time: {nepal}, Indian Time: {india} ")
     print("")
     return True
