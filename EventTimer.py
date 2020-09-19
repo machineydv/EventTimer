20cd6cd18ae53fe39b83fb9fd76b35c43a24fb0e
machinexa <machinexa@gmail.com>
2020-09-19 19:51:35 +0545

    07:51:35 PM 09/19/2020

--- EventTimer.py
+++ EventTimer.py
@@ -31,24 +31,24 @@ def CounterSeconds(edict):
     date, seconds = date_from_seconds(event_seconds-current_seconds)
     counter_seconds = seconds_from_date(date) + int(seconds)
     return counter_seconds
+if __name__ == '__main__':
+    counter_1 = CounterSeconds(event_1)
+    counter_2 = CounterSeconds(event_2)
+    counter_3 = CounterSeconds(event_3)
+    counter_4 = CounterSeconds(event_4)
 
-counter_1 = CounterSeconds(event_1)
-counter_2 = CounterSeconds(event_2)
-counter_3 = CounterSeconds(event_3)
-counter_4 = CounterSeconds(event_4)
-
-try:
-    while True:
-        print("\x1b[%d;%dH" % (1, 1), end="")
-        print_from_seconds(counter_1, event_1)
-        print_from_seconds(counter_2, event_2)
-        print_from_seconds(counter_3, event_3)
-        print_from_seconds(counter_4, event_4)
-        counter_1 -= 1
-        counter_2 -= 1
-        counter_3 -= 1
-        counter_4 -= 1
-        sleep(1)
-except KeyboardInterrupt:
-    print(f"{ColorObj.bad} Exiting")
-    exit()
+    try:
+        while True:
+            print("\x1b[%d;%dH" % (1, 1), end="")
+            print_from_seconds(counter_1, event_1)
+            print_from_seconds(counter_2, event_2)
+            print_from_seconds(counter_3, event_3)
+            print_from_seconds(counter_4, event_4)
+            counter_1 -= 1
+            counter_2 -= 1
+            counter_3 -= 1
+            counter_4 -= 1
+            sleep(1)
+    except KeyboardInterrupt:
+        print(f"{ColorObj.bad} Exiting")
+        exit()
