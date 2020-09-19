2640080e01464ea44ad9183b9347450b173c1fb2
machinexa <machinexa@gmail.com>
2020-09-19 19:45:53 +0545

    07:45:53 PM 09/19/2020

--- lib/PrintFunctions.py
+++ lib/PrintFunctions.py
@@ -9,8 +9,9 @@ def print_from_seconds(second, edict):
     India = colored(time_from_seconds(second-seconds_from_time('00:15:00')), color='yellow')
     Universal = colored(time_from_seconds(second-seconds_from_time('05:45:00')), color='red')
     if 'event_description' in edict.keys():
-        print(f"{ColorObj.information} Event {edict['event_name']}:{edict['event_description']}")
+        print(f"{ColorObj.information} Event {colored(edict['event_name'], color='green', attrs=['bold'])}: {edict['event_description']}")
     else:
-        print(f"{ColorObj.information} Event: {edict['event_name']}")
+        print(f"{ColorObj.information} Event: {colored(edict['event_name'], color='green', attrs=['bold'])}")
     print(f"{ColorObj.good} Universal Time: {Universal}, Nepali Time: {Nepal}, Indian Time: {India} ")
+    print("")
     return True
