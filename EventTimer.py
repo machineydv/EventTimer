eeee119be8ae77b537cf92f5c115580ccc0a3006
machinexa <machinexa@gmail.com>
2020-09-19 09:41:13 +0545

    09:41:12 AM 09/19/2020

--- EventTimer.py
+++ EventTimer.py
@@ -33,7 +33,7 @@ def CounterSeconds(edict):
 counter_1 = CounterSeconds(event_1)
 counter_2 = CounterSeconds(event_2)
 counter_3 = CounterSeconds(event_3)
-#counter_4 = CounterSeconds(event_4)
+counter_4 = CounterSeconds(event_4)
 
 try:
     while True:
@@ -41,11 +41,11 @@ try:
         print_from_seconds(counter_1, event_1)
         print_from_seconds(counter_2, event_2)
         print_from_seconds(counter_3, event_3)
-#        print_from_seconds(counter_4, event_4)
+        print_from_seconds(counter_4, event_4)
         counter_1 -= 1
         counter_2 -= 1
         counter_3 -= 1
-#        counter_4 -= 1
+        counter_4 -= 1
         sleep(1)
 except KeyboardInterrupt:
     print(f"{ColorObj.bad} Exiting")
